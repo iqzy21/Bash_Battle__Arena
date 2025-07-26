d1027b6b00f6ef96557546b3616eb0cf0c417e83
iqbaal elhajoui <iqbaal.el21@gmail.com>
2025-07-26 20:39:45 +0100

    added mission / task

--- Bash Battle Arena/Level-1.sh
+++ Bash Battle Arena/Level-1.sh
@@ -8,3 +8,11 @@ do
 
 done
 
+: ' 
+Level 1: The Basics
+
+Mission: Create a directory named Arena and then inside it, create three files: warrior.txt, mage.txt, and archer.txt. List the contents of the Arena directory.
+
+'
+
+
