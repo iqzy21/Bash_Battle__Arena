c9cee22a8e4827bbcdc6f9eb0cb14bcb800e73a5
iqbaal elhajoui <iqbaal.el21@gmail.com>
2025-07-27 02:58:44 +0100

    Completed Level 7 solution :)

--- Bash Battle Arena/Level-7.sh
+++ Bash Battle Arena/Level-7.sh
@@ -12,3 +12,6 @@ echo "Sorted files by size"
 
 
 sort_file
+#Mission: Write a script that sorts all .txt files 
+#in a directory by their size, from smallest to largest, 
+#and displays the sorted list
